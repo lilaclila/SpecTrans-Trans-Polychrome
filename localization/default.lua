72191c577d56e119d33f46d9373db83bb531b1fc
lilaclila <thrasherglitches@gmail.com>
2026-06-03 23:37:32 +0100

    Update default.lua

--- localization/default.lua
+++ localization/default.lua
@@ -6,6 +6,91 @@ return {
         }
     },
     descriptions = {
+        Edition = {
+            e_polychrome = {
+                name = "Trans",
+                text = {
+                    "{X:mult,C:white} X#1# {} Mult",
+                },
+            },
+        },
+        Joker = {
+            j_bootstraps = {
+                name = "Bootstraps",
+                text = {
+                    "{C:mult}+#1#{} Mult for every",
+                    "{C:money}$#2#{} you have",
+                    "{C:inactive}(Currently {C:mult}+#3#{C:inactive} Mult)",
+                },
+                unlock = {
+                    "Have at least {E:1,C:attention}#1#",
+                    "{C:dark_edition}Trans{} Jokers",
+                },
+            },
+        },
+        Spectral = {
+            c_aura = {
+                name = "Aura",
+                text = {
+                    "Add {C:dark_edition}Foil{}, {C:dark_edition}Holographic{},",
+                    "or {C:dark_edition}Trans{} effect to",
+                    "{C:attention}1{} selected card in hand",
+                },
+            },
+            c_hex = {
+                name = "Hex",
+                text = {
+                    "Add {C:dark_edition}Trans{} to a",
+                    "random {C:attention}Joker{}, destroy",
+                    "all other Jokers",
+                },
+            },
+        },
+        Tag = {
+            tag_polychrome = {
+                name = "Trans Tag",
+                text = {
+                    "Next base edition shop",
+                    "Joker is free and",
+                    "becomes {C:dark_edition}Trans",
+                },
+            },
+        },
+        Tarot = {
+            c_wheel_of_fortune = {
+                name = "The Wheel of Fortune",
+                text = {
+                    "{C:green}#1# in #2#{} chance to add",
+                    "{C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, or",
+                    "{C:dark_edition}Trans{} edition",
+                    "to a random {C:attention}Joker",
+                },
+            },
+        },
+        Voucher = {
+            v_glow_up = {
+                name = "Glow Up",
+                text = {
+                    "{C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, and",
+                    "{C:dark_edition}Trans{} cards",
+                    "appear {C:attention}#1#X{} more often",
+                },
+                unlock = {
+                    "Have at least {C:attention}#1#",
+                    "{C:attention}Joker{} cards with",
+                    "{C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, or",
+                    "{C:dark_edition}Trans{} edition",
+                },
+            },
+            v_hone = {
+                name = "Hone",
+                text = {
+                    "{C:dark_edition}Foil{}, {C:dark_edition}Holographic{}, and",
+                    "{C:dark_edition}Trans{} cards",
+                    "appear {C:attention}#1#X{} more often",
+                },
+            },
+        },
         alt_texture = {
             alt_tex_tspa_spectral = {
                 c_familiar = { badge = 'spectrans_card'},
